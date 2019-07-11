dc628926e281b1026060c8d6a1c76dfeda0a0a16
wry5560 <233089043@qq.com>
2019-07-12 01:57:34 +0800

    apis

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,11 @@ export default {
   data () {
     return {
       websock: null,
-      tradeTableData: []
+      setIntervalPingPong:null,
+      setTimeoutReset:null,
+      lockReconnect:false,
+
+      tradeTableData: [],
     }
   },
   created () {
@@ -53,31 +57,77 @@ export default {
   },
   methods: {
     initWebSocket () { // 初始化weosocket
-      const wsuri = 'wss://www.bitmex.com/realtime'
-      this.websock = new WebSocket(wsuri)
-      this.websock.onmessage = this.websocketonmessage
-      this.websock.onopen = this.websocketonopen
-      this.websock.onerror = this.websocketonerror
-      this.websock.onclose = this.websocketclose
+
+      if (!this.websock) {
+        console.log('建立websocket连接')
+        const wsuri = 'wss://www.bitmex.com/realtime'
+        this.websock = new WebSocket(wsuri)
+        this.websock.onmessage = this.websocketonmessage
+        this.websock.onopen = this.websocketonopen
+        this.websock.onerror = this.websocketonerror
+        this.websock.onclose = this.websocketclose
+      } else {
+        console.log('websocket已连接')
+      }
     },
     websocketonopen () { // 连接建立之后执行send方法发送数据
-      console.log('bitMex ws is open!')
-      this.websock.send('ping')
+      console.log('websocket已连接')
+      this.setIntervalWs()
       // let actions = { 'test': '12345' }
       // this.websocketsend(JSON.stringify(actions))
     },
-    websocketonerror () { // 连接建立失败重连
-      this.initWebSocket()
+    websocketonerror (e) { // 连接建立失败重连
+      clearInterval(this.setIntervalPingPong)
+      console.log('链接错误:'+ JSON.parse(e))
+      this.websock.close()
+      this.websock=null
+      this.reconnect()
     },
     websocketonmessage (e) { // 数据接收
       // const redata = JSON.parse(e.data)
-      console.log(e.data)
+      clearInterval(this.setIntervalPingPong)
+      clearTimeout(this.setTimeoutReset)
+      this.setIntervalWs()
+      console.log(JSON.parse(e.data))
     },
     websocketsend (Data) { // 数据发送
+      if (this.websock !== null && this.websock.readyState === 3) {
+        clearInterval(this.setIntervalPingPong)
+        this.websock.close()
+        this.websock=null
+        this.reconnect() //重连
+      } else if (this.websock.readyState === 1) {
+        this.websock.send(Data)
+      } else if (this.websock.readyState === 0) {
+        setTimeout(() => {
+          this.websocketsend(Data)
+        }, 3000)
+      }
       this.websock.send(Data)
     },
     websocketclose (e) { // 关闭
-      console.log('断开连接', e)
+      console.log('断开连接'+JSON.parse(e))
+    },
+    setIntervalWs (){
+      const _this=this
+      this.setIntervalPingPong=setInterval(()=>{
+        _this.websock.send('ping')
+        _this.setTimeoutReset=setTimeout(()=>{
+          clearInterval(_this.setIntervalPingPong)
+          _this.websock.close()
+          _this.websock=null
+          _this.reconnect()
+        },5000)
+      },5000)
+    },
+    reconnect(){
+      const _this=this
+      if(this.lockReconnect) return
+      this.lockReconnect = true
+      setTimeout(function () {     //没连接上会一直重连，设置延迟避免请求过多
+        _this.initWebSocket();
+        _this.lockReconnect = false;
+      }, 2000);
     }
   }
 }
